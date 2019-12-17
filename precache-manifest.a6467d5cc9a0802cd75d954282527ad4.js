c1fd7cb95202a061880f2b1555e143e93f706ad2
Artemy Magarin <magar.art@yandex.ru>
2019-12-17 03:13:58 +0300

    rev 1.0

---
+++ precache-manifest.a6467d5cc9a0802cd75d954282527ad4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f20c5ce07b1b553ad130c88f42c592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e712b6e9e5a9b74ee58",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "cab2732263e804944cf2",
+    "url": "/static/css/main.540cba97.chunk.css"
+  },
+  {
+    "revision": "3e712b6e9e5a9b74ee58",
+    "url": "/static/js/2.62c08bc2.chunk.js"
+  },
+  {
+    "revision": "8fa3f2c2541d0aa1b68abe84a745cebb",
+    "url": "/static/js/2.62c08bc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cab2732263e804944cf2",
+    "url": "/static/js/main.3fa36295.chunk.js"
+  },
+  {
+    "revision": "9b6995e74ab6c5af29c8",
+    "url": "/static/js/runtime-main.ac97cd4d.js"
+  },
+  {
+    "revision": "5c12dca936f64cdf4e7e32a2bf0ff655",
+    "url": "/static/media/back.5c12dca9.svg"
+  },
+  {
+    "revision": "9e90c003aade2e5ed1f377d29c1e95ce",
+    "url": "/static/media/multiply.9e90c003.svg"
+  },
+  {
+    "revision": "6d46adfa8765eef6df314bbd5fddc265",
+    "url": "/static/media/next.6d46adfa.svg"
+  },
+  {
+    "revision": "b052df94f49f3f4b4c8cc98baf441c0c",
+    "url": "/static/media/photo.b052df94.jpg"
+  }
+]);
