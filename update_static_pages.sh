4e2605091a69a1b23afc183317c1ae366d4487c4
Artemy Magarin <magar.art@yandex.ru>
2020-06-21 19:12:23 +0300

    update

---
+++ update_static_pages.sh
@@ -0,0 +1,7 @@
+cp index.html about.html
+cp index.html projects/font-in-the-environment.html
+cp index.html projects/grid-font.html
+cp index.html projects/image-and-typography.html
+cp index.html projects/materials-research.html
+cp index.html projects/proverb-visualization.html
+cp index.html projects/subject-and-font.html
