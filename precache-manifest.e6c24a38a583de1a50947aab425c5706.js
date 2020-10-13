b648f37afe78c6b71c59d928d51b98917f931da4
Artemy Magarin <magar.art@yandex.ru>
2020-10-13 12:07:46 +0300

    Updated layout

---
+++ precache-manifest.e6c24a38a583de1a50947aab425c5706.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409a7d3eab4dec5278208e924a75fd5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6842c650b1b2abec860",
+    "url": "/static/css/2.cff28e81.chunk.css"
+  },
+  {
+    "revision": "b72d7e119d8b5b979796",
+    "url": "/static/css/main.98046aa7.chunk.css"
+  },
+  {
+    "revision": "e6842c650b1b2abec860",
+    "url": "/static/js/2.2706554c.chunk.js"
+  },
+  {
+    "revision": "d7b40c5eff7461ee4d9ffd485b3aaad8",
+    "url": "/static/js/2.2706554c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b72d7e119d8b5b979796",
+    "url": "/static/js/main.d43e51e7.chunk.js"
+  },
+  {
+    "revision": "9b6995e74ab6c5af29c8",
+    "url": "/static/js/runtime-main.ac97cd4d.js"
+  },
+  {
+    "revision": "5c12dca936f64cdf4e7e32a2bf0ff655",
+    "url": "/static/media/back.5c12dca9.svg"
+  },
+  {
+    "revision": "9e90c003aade2e5ed1f377d29c1e95ce",
+    "url": "/static/media/multiply.9e90c003.svg"
+  },
+  {
+    "revision": "6d46adfa8765eef6df314bbd5fddc265",
+    "url": "/static/media/next.6d46adfa.svg"
+  },
+  {
+    "revision": "b102f636b526a502f868eed2b291c0f9",
+    "url": "/static/media/photo.b102f636.jpg"
+  }
+]);
